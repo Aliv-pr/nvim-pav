70aeffa8ec853c22fb28dd8f2f112c82179c2f2d
Iván PR <alex2408rosales@gmail.com>
2024-10-07 15:20:45 -0600

    Configure para el servidor typescript

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = {"lua_ls"}
+        ensure_installed = {"lua_ls", "tsserver"}
       })
     end
   },
@@ -18,6 +18,7 @@ return {
     config = function()
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({}) --lua
+      lspconfig.ts_ls.setup({}) --typescript
       -- lspconfig.tsserver.setup({}) --typescript
 
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
