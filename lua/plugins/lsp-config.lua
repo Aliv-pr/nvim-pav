a8c08bddc077e4d3defb809db5080d2cbeb02168
Iván PR <alex2408rosales@gmail.com>
2024-10-07 15:07:30 -0600

    Elimine el tsserver porque estaba deprecated

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = {"lua_ls", "tsserver"}
+        ensure_installed = {"lua_ls"}
       })
     end
   },
@@ -18,7 +18,8 @@ return {
     config = function()
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({}) --lua
-      lspconfig.tsserver.setup({}) --typescript
+      -- lspconfig.tsserver.setup({}) --typescript
+
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
       vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
