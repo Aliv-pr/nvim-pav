cf0fd69546e5466d78164b954387033132bd74d7
Aliv-pr <alex2408rosales@gmail.com>
2026-02-09 09:45:01 -0600

    cambiamos treesitter para que descargue cuando sea necesario un nuevo analizador dependiendo del lenguaje

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,10 +4,10 @@ return {
   config = function()
     local config = require("nvim-treesitter.configs")
     config.setup({
-      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "bibtex", "cmake", "typescript" },
+      auto_install = true,
       sync_install = false,
       highlight = { enable = true },
-      indent = { enable = true },  
+      indent = { enable = true },
     })
-  end
+  end,
 }
